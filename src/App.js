387da3646752874636630812069e3bab159e9e22
Diego Andres Cabrera Rojas <diegoandresrojas2000@gmail.com>
2020-12-09 11:02:04 -0500

    creando enrutamiento

--- src/App.js
+++ src/App.js
@@ -1,7 +1,29 @@
+import React from "react";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import EditarProducto from "./components/EditarProducto";
 import Header from "./components/Header";
+import NuevoProducto from "./components/NuevoProducto";
+import Productos from "./components/Productos";
 
 function App() {
-  return <Header />;
+  return (
+    <>
+      <Router>
+        <Header />
+        <div className="container">
+          <Switch>
+            <Route exact path="/" component={Productos} />
+            <Route exact path="/productos/nuevo" component={NuevoProducto} />
+            <Route
+              exact
+              path="/productos/editar/:id"
+              component={EditarProducto}
+            />
+          </Switch>
+        </div>
+      </Router>
+    </>
+  );
 }
 
 export default App;
---
+++ src/components/EditarProducto.js
@@ -0,0 +1,6 @@
+import React from "react";
+const EditarProducto = () => {
+  return <h1>Edicion Producto</h1>;
+};
+
+export default EditarProducto;
